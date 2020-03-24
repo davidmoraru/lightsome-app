022ec0f02e1ad840bdb1f89bc44454abad0947b2
James <james@gmail.com>
2020-03-24 04:16:09 +0200

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -1,1 +1,26 @@
 <?php
+
+// Use / for the main/index page.
+get('/', function(){
+    echo "Front page";
+});
+
+// All routes start with /
+get('/about', function(){
+    echo "About page";
+});
+
+// Regex enabled, groups get passed to the function.
+get('/projects/([a-zA-Z0-9\-_]+)', function($project){
+    echo "Project page for {$project}";
+});
+
+// Handle a POST request
+post('/contact', function(){
+    // Handle submitted contact form.
+});
+
+// Checking if a route has been matched
+if (!Nanite::$routeProccessed) {
+    // 404 page here
+}
